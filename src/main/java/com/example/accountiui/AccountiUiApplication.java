e19d7e4f91ae2eb2c0b76829920b9eefa36146f7
anura <anurag89207@gmail.com>
2026-02-01 11:14:23 +0530

    first commit v1

---
+++ src/main/java/com/example/accountiui/AccountiUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountiui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountiUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountiUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountiui/AccountiUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountiui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountiUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
